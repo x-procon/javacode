69e45b661b88a65df3010b54733dd7520dcc1403
huangpg <huangpg@bairuitech.com>
2019-12-20 17:57:48 +0800

    redis test

---
+++ study/redis/src/main/java/hazer/test/redis/PingTest.java
@@ -0,0 +1,17 @@
+package hazer.test.redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * @ClassName: PingTest
+ * @Author: Hazer
+ * @Description: 测试redis连接
+ * @Date: 2019/12/20 17:01
+ * @Version: 1.0
+ */
+public class PingTest {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("122.51.94.168",6379);
+        System.out.println(jedis.ping());
+    }
+}
