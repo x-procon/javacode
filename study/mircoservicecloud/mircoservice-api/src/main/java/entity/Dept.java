28d31e1d27e81d0db53a506e37f6bf719196cb46
huangpg <huangpg@bairuitech.com>
2020-01-14 19:20:10 +0800

    springcloud 搭建1

---
+++ study/mircoservicecloud/mircoservice-api/src/main/java/entity/Dept.java
@@ -0,0 +1,27 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName Dept
+ * @Description TODO
+ * @Author Hazer
+ * @Date 2020/1/13 11:56
+ **/
+@SuppressWarnings(value = "serial")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private Long deptNo;
+    private String deptname;
+    private String dbSource;
+
+}
