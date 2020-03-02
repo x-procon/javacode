620f3d9c8b2afc49ce0429db4dc3bb55da43a0d2
Gyo Howar <1187147080@qq.com>
2020-03-02 22:06:24 +0800

    项目环境搭建

--- shop_parent/shop_service_api/shop_service_goods_api/src/main/java/com/shop/goods/service/BrandService.java
+++ shop_parent/shop_service_api/shop_service_goods_api/src/main/java/com/shop/goods/service/BrandService.java
@@ -1,8 +1,10 @@
 package com.shop.goods.service;
 
+import com.gyo.shop.entity.Page;
 import com.shop.goods.pojo.Brand;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandService {
     /**
@@ -34,4 +36,28 @@ public interface BrandService {
      * @param id
      */
     void delete(Integer id);
+
+    /***
+     * 多条件搜索品牌方法
+     * @param searchMap
+     * @return
+     */
+    public List<Brand> findList(Map<String, Object> searchMap);
+
+    /***
+     * 分页查询
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Brand> findPage(int page, int size);
+
+    /***
+     * 多条件分页查询
+     * @param searchMap
+     * @param page
+     * @param size
+     * @return
+     */
+    Page<Brand> findPage(Map<String, Object> searchMap, int page, int size);
 }
--- shop_parent/shop_service_api/shop_service_goods_api/src/main/java/com/shop/goods/service/impl/BrandServiceImpl.java
+++ shop_parent/shop_service_api/shop_service_goods_api/src/main/java/com/shop/goods/service/impl/BrandServiceImpl.java
@@ -1,19 +1,25 @@
 package com.shop.goods.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.gyo.shop.entity.Page;
 import com.shop.goods.dao.BrandMapper;
 import com.shop.goods.pojo.Brand;
 import com.shop.goods.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BrandServiceImpl implements BrandService {
     @Autowired
     private BrandMapper brandMapper;
+
     /**
      * 查询全部列表
+     *
      * @return
      */
     @Override
@@ -23,40 +29,105 @@ public class BrandServiceImpl implements BrandService {
 
     /**
      * 根据ID查询
+     *
      * @param id
      * @return
      */
     @Override
-    public Brand findById(Integer id){
-        return  (Brand) brandMapper.selectByPrimaryKey(id);
+    public Brand findById(Integer id) {
+        return (Brand) brandMapper.selectByPrimaryKey(id);
     }
 
 
     /**
      * 增加
+     *
      * @param brand
      */
     @Override
-    public void add(Brand brand){
+    public void add(Brand brand) {
         brandMapper.insert(brand);
     }
 
 
     /**
      * 修改
+     *
      * @param brand
      */
     @Override
-    public void update(Brand brand){
+    public void update(Brand brand) {
         brandMapper.updateByPrimaryKey(brand);
     }
 
     /**
      * 删除
+     *
      * @param id
      */
     @Override
-    public void delete(Integer id){
+    public void delete(Integer id) {
         brandMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 条件查询
+     *
+     * @param searchMap
+     * @return
+     */
+    @Override
+    public List<Brand> findList(Map<String, Object> searchMap) {
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+        if (searchMap != null) {
+            // 品牌名称
+            if (searchMap.get("name") != null &&
+                    !"".equals(searchMap.get("name"))) {
+                criteria.andLike("name", "%" + searchMap.get("name") + "%");
+            }
+            // 品牌的首字母
+            if (searchMap.get("letter") != null &&
+                    !"".equals(searchMap.get("letter"))) {
+                criteria.andEqualTo("letter", searchMap.get("letter"));
+            }
+        }
+        return brandMapper.selectByExample(example);
+    }
+
+
+    @Override
+    public Page<Brand> findPage(int page, int size) {
+        PageHelper.startPage(page, size);
+        return (Page<Brand>) brandMapper.selectAll();
+    }
+
+    /**
+     * 条件+分页查询
+     *
+     * @param searchMap 查询条件
+     * @param page      页码
+     * @param size      页大小
+     * @return 分页结果
+     */
+    @Override
+    public Page<Brand> findPage(Map<String, Object> searchMap, int page, int
+            size) {
+        PageHelper.startPage(page, size);
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+        if (searchMap != null) {
+            // 品牌名称
+            if (searchMap.get("name") != null &&
+                    !"".equals(searchMap.get("name"))) {
+                criteria.andLike("name", "%" + searchMap.get("name") + "%");
+            }
+            // 品牌的首字母
+            if (searchMap.get("letter") != null &&
+                    !"".equals(searchMap.get("letter"))) {
+                criteria.andEqualTo("letter", searchMap.get("letter"));
+            }
+        }
+        return (Page<Brand>) brandMapper.selectByExample(example);
     }
 }
