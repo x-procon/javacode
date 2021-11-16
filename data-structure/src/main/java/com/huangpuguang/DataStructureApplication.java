21b9efe05fa55ba48e03c0bd39e62ccc0415da89
HPG <xprocon@163.com>
2021-11-16 15:32:18 +0800

    feat：数据结构项目

---
+++ data-structure/src/main/java/com/huangpuguang/DataStructureApplication.java
@@ -0,0 +1,16 @@
+package com.huangpuguang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Procon
+ */
+@SpringBootApplication
+public class DataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureApplication.class, args);
+    }
+
+}
